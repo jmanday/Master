a389aac207edd6449a518ad510ff7cd9a614ad59
Jesus Garcia Manday <jmanday@gmail.com>
2017-06-04 13:21:11 +0200

    eliminados ficheros innecesarios en TID

--- TID/Ficheros script R/Tratamiento de datos/generacsv.R
+++
@@ -1,6 +0,0 @@
-var1=1:5
-var2=(1:5)/10
-var3=c("R","and","data mining","Examples","Case Estudies")
-df1=data.frame(var1,var2,var3)
-names(df1)=c("Enteros","Reales","String")
-write.csv(df1,"./Ejemplos manejo de datos/Data/tontos.csv",row.names=FALSE) 
